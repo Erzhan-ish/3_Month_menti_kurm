6b207c5110dcd749eacf5814ed036dc7b1339527
Erzhan-ish <isenbekoverzan1@gmail.com>
2025-02-21 16:05:05 +0600

    hw_3

--- bot_config.py
+++ bot_config.py
@@ -1,10 +1,12 @@
 from aiogram import Dispatcher, Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from decouple import config
 
 token = config('BOT_TOKEN')
 
+storage = MemoryStorage()
 bot = Bot(token=token)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
 
 admins = [5747517813,]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aiogram import executor
 import logging
 from bot_config import dp, bot, admins
-from handlers import commands, echo, quiz
+from handlers import commands, echo, quiz, FSM_registration, FSM_store
 
 async def on_startup(_):
     for admin in admins:
@@ -16,6 +16,8 @@ async def on_shutdown(_):
 if __name__ == '__main__':
     commands.register_handlers(dp)
     quiz.register_handlers(dp)
+    FSM_registration.register_handlers_fsm(dp)
+    FSM_store.register_handlers_fsm(dp)
     echo.register_handlers(dp)
 
     logging.basicConfig(level=logging.INFO)
